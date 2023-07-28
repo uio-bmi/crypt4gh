e0a44572e6e63940b2eaa334e444f11920490f4e
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-07-28 15:53:10 +1000

    Remove shaded plugins since it breaks java module system isolation (involves renaming/moving/etc...). Also comment out compiler plugins and works toward publishability, warning: Required filename-based automodules detected: [blake2b-1.0.0.jar, commons-cli-1.5.0.jar, bkdf-0.6.0.jar, bcrypt-0.10.2.jar, scrypt-1.4.0.jar]. Please don't publish this project to a public artifact repository

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,18 @@
 module no.uio.ifi.crypt4gh {
     requires blake2b;
-    requires lombok;
     requires org.apache.commons.lang3;
     requires org.apache.commons.io;
     requires bcrypt;
     requires scrypt;
     requires bkdf;
     requires commons.cli;
+    requires org.slf4j;
+    requires lombok;
 
-//    exports no.uio.ifi.crypt4gh;
     exports no.uio.ifi.crypt4gh.stream;
     exports no.uio.ifi.crypt4gh.pojo;
+    exports no.uio.ifi.crypt4gh.pojo.header;
+    exports no.uio.ifi.crypt4gh.pojo.body;
+    exports no.uio.ifi.crypt4gh.pojo.key;
     exports no.uio.ifi.crypt4gh.util;
 }
