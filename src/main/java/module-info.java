7652b7ec49c7e099a354567e8fcb87017eca9cde
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-11-14 00:23:12 +1100

    Add module-info.java to ship .jar as >=JDKv9 modular libraries (#87)
    * Add module-info.java for JDK>=11 compat (see: https://dev.java/learn/modules/intro/)

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module no.uio.ifi.crypt4gh {
+    requires blake2b;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.io;
+    requires bcrypt;
+    requires scrypt;
+    requires bkdf;
+    requires commons.cli;
+    requires org.slf4j;
+    requires lombok;
+
+    exports no.uio.ifi.crypt4gh.stream;
+    exports no.uio.ifi.crypt4gh.pojo;
+    exports no.uio.ifi.crypt4gh.util;
+}
